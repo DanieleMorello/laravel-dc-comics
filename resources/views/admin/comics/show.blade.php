050170b6353d77aea74b6e16636c705d3a1be92e
DanieleMorello <danielemorello@live.it>
2023-06-01 15:09:42 +0200

    Dalete card in Show Page and change style

--- resources/views/admin/comics/show.blade.php
+++ resources/views/admin/comics/show.blade.php
@@ -3,22 +3,13 @@
 @section('page_title', 'Show')
 
 @section('content')
-    <div class="container py-5">
-        <div class="d-flex justify-content-between align-items-center">
-            <h1 class="py-3">{{ $comic->title }}</h1>
-            <a name="" id="" class="btn btn-primary ml-auto" href="{{ route('admin.comics.index') }}"
-                role="button">
-                Return Back</a>
+    <div class="container d-flex p-3">
+        <img class="img-fluid" src="{{ $comic->thumb }}" alt="">
+        <div class="ps-5">
+            <h1>{{ $comic->title }}</h1>
+            <p>{{ $comic->description }}</p>
         </div>
-        <div class="col-6 m-auto">
-            <div class="card">
-                <img class="card-img-top" src="{{ $comic->thumb }}" alt="{{ $comic->title }}">
-                <div class="card-body">
-                    <h4 class="card-title">{{ $comic->title }}</h4>
-                    <p class="card-text">{{ $comic->description }}</p>
-                </div>
-            </div>
-        </div>
+
     </div>
 
 @endsection
--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -4,9 +4,6 @@
     <div class="container">
         <img class="img-fluid" src="{{ $comic->thumb }}" alt="">
         <h1>{{ $comic->title }}</h1>
-        <p>
-            {{ $comic->description }}
-
-        </p>
+        <p>{{ $comic->description }}</p>
     </div>
 @endsection
